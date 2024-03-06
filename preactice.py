1b7421a389df9b201d90506b4da85f995ad1e74e
Shrey <mishra.shrey001@gmail.com>
2024-03-06 16:42:58 +0530

    Completed till #10of100days

--- preactice.py
+++ preactice.py
@@ -1,5 +1,11 @@
-num = int(input("Enter the number to check : "))
-if(num%2==0):
-    print("The number is even")
-else:
-    print("The number is odd")
+# num = int(input("Enter the number to check : "))
+# if(num%2==0):
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+
+
+x = 10 
+y = 50 
+if (x ** 2 > 100 and y < 100): print(x, y)
---
+++ typecasting.py
@@ -0,0 +1,10 @@
+a = "1"
+print(type(a))
+a = int(a)
+
+print(type(a))
+
+#types of casting : implicit and explicit casting
+
+
+
