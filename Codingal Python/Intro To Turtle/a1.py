78166c97b96ab6496b8b1ec7cf2b4bef8d321e50
Shrey <mishra.shrey001@gmail.com>
2024-03-29 19:14:57 +0530

    minor changes

--- Codingal Python/Intro To Turtle/a1.py
+++ Codingal Python/Intro To Turtle/a1.py
@@ -1,7 +1,7 @@
 import turtle
 
 # creating canvas
-turtle.Screen().bgcolor("Orange")
+turtle.Screen().bgcolor("Red")
 
 sc = turtle.Screen()
 sc.setup(400, 300)
@@ -12,8 +12,12 @@ turtle.title("Welcome to Turtle Window")
 board = turtle.Turtle()
 
 # creating a square
-for i in range(4):
-	board.forward(100)
-	board.left(90)
-	i = i+1
+for i in range(2):
+    board.forward(100)
+    board.left(90)
+    board.forward(200)
+    board.left(90)
+	
 
+
+turtle.done()
